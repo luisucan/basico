29ab10f8fa0f58707706c0f7aa3ea230935ae11e
luisucan <luis_alberto_ucan@hotmail.com>
2019-05-30 13:48:13 -0500

    socket angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+
+const config: SocketIoConfig = { 
+  url: environment.wsUrl, options: {} 
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
